e064e0d29b664f9a80232193cf4bc05f7d7a55b1
ayushdubey7799 <113593098+ayushdubey7799@users.noreply.github.com>
2023-05-18 19:04:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const select = document.getElementById('colorSelect');
+
+const options = document.getElementById('colorSelect').children;
+ function remove() {
+	
+ 	for(x of options){
+		if(x.value == select.value){
+			
+			x.remove();
+		}
+	}
+ }
